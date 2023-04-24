9f7bc4c8549f34f7e7675f4cbeddcebfcc8b6b38
simon y <71997308+kjai139@users.noreply.github.com>
2023-04-24 19:53:55 -0400

    skeleton

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+app.get('/scape', async (req, res) => {
+    try {
+        const url = req.query.url
+
+        //fetch html of page
+        const { data } = await axios.get(url)
+
+        //load html into cheerio
+        const $ = cheerio.load(data)
+
+        //extract desired data from html
+        const title = $('title').text()
+        const metaDescription = $('meta[name="description"]').attr('content')
+
+        res.json({
+            title,
+            metaDescription
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: 'Internal server error'})
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000')
+})
---
+++ frontend/src/components/scrapper.js
@@ -0,0 +1,7 @@
+
+
+const Scrapper = () => {
+    
+}
+
+export default Scrapper
