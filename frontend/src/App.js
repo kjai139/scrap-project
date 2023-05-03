5ebe0e0e70a8c3fd3b47bef51cf4af34030b8e9a
simon y <71997308+kjai139@users.noreply.github.com>
2023-05-03 18:48:01 -0400

    results display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Scrapper from './components/scrapper';
 function App() {
   return (
     <div className="App">
-      <h1>THE SCRAPER</h1>
+      
       <Scrapper></Scrapper>
     </div>
   );
--- frontend/src/components/scrapper.js
+++ frontend/src/components/scrapper.js
@@ -6,6 +6,9 @@ import axios from 'axios'
 const Scrapper = () => {
 
     const [directory, setDirectory] = useState()
+
+    const [pageTitle, setPageTitle] = useState()
+    const [pageParagraphs, setPageParagraphs] = useState()
     const inputUrl = useRef(null)
     const instance = axios.create({
         baseURL: 'http://localhost:4000'
@@ -21,6 +24,10 @@ const Scrapper = () => {
             }
         }).then(response => {
             console.log(response.data)
+            setPageTitle(response.data.title)
+            setPageParagraphs(response.data.paragraphs)
+            console.log(response.data.paragraphs)
+            console.log(pageParagraphs)
         }).catch(err => {
             console.error(err)
         })
@@ -28,12 +35,32 @@ const Scrapper = () => {
     }
 
     return (
-        <div>
-            <form>
-            <label htmlFor='url-input'></label>
+        <div className='App'>
+            <h1 style={{padding: '1rem'}}>THE SCRAPER</h1>
+            <form className='scrap-form' style={{padding: '1rem'}}>
+            <label htmlFor='url-input'>Type in full Url (including https://)</label>
+            <div style={{display: 'flex', gap:'5px'}}>
             <input id='url-input' type="text" ref={inputUrl}></input>
-            <button onClick={handleSubmit}>Scrape</button>
+            <button onClick={handleSubmit} className='scrap-btn'>Scrape</button>
+            </div>
             </form>
+            <div className='resultDiv'>
+                <h2>RESULTS</h2>
+                <h3 className='result-title'>Page Title</h3>
+                <span>{pageTitle}</span>
+                <div className='result-para-div'>
+                    
+                    {pageParagraphs && pageParagraphs.map((node, id) => {
+                        return <div className='pagePara-div'>
+                            <h4 style={{margin: '1rem 0rem 1rem 0rem'}}>{`BLOCK #${id}`}</h4>
+                            <button className='para-btns' key={id} >{node}</button>
+                            </div>
+                        
+                    })}
+
+                </div>
+                
+            </div>
         </div>
     )
 }
